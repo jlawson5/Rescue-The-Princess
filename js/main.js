88af6ed132493f762dd75c04e1bd178fb95a303f
jlawson5 <jlawson5@masonlive.gmu.edu>
2016-02-17 17:50:20 -0500

    Coded just about the entire game
    Progress!

--- js/main.js
+++ js/main.js
@@ -6,187 +6,188 @@
 
     window.onload = function() {
 
-        var game = new Phaser.Game(600, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+        var game = new Phaser.Game(600, 400, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
 
-    game.load.image('platform', 'assets/GravityManPlatform.png');//Floor and Platform objects are not working properly//
-    game.load.image('floor', 'assets/GravityManFloor.png');
-    game.load.spritesheet('gravityMan', 'assets/GravityMan.png', 32, 32);//Gravity Man: The player character//
-    game.load.image('coin', 'assets/GravityManCoin.png');//Coins the player must find and collect//
-    game.load.image('background', 'assets/GravityManBG.png');//Background image//
-    game.load.audio('coinCollect', 'assets/Electronic_Chime-KevanGC-495939803.mp3');
-    game.load.audio('gravityChange', 'assets/spin_jump-Brandino480-2020916281.mp3');
+    game.load.image('floor', 'assets/Block.png');
+    game.load.spritesheet('knight', 'assets/Knight.png', 32, 40);//Knight: Player Character//
+    game.load.spritesheet('slimeSmall', 'assets/SlimeSmall.png', 32, 32);//Small Slime: Basic slime, jump over or kill//
+    game.load.spritesheet('slimeHeavy', 'assets/SlimeMedium.png', 32, 32);//Heavy Slime: Cannot be killed//
+    game.load.spritesheet('slimeFlying', 'assets/SlimeFlying.png', 32, 32);//Flying Slime: Cannot be jumped over//
+    game.load.image('princess', 'assets/Princess.png');//Princess: Reach the princess to win the game//
+    game.load.image('arrow', 'assets/Arrow.png');//Arrow: Projectile shot by player//
 }
 
 var player;
-var facing = 'left';
 var jumpTimer = 0;
 var cursors;
-var jumpButton;
+var shootButton;
 var bg;
-var g_dir = 'down';//current direction of gravity//
-var coinSound;
-var gSound;
-var coins;
-var score = 0;
-var scoreString = '';
-var scoreText;
-var floor;//Not working//
-
+var distance = 5000;
+var distString = '';
+var distText;
+var slimes;
+var floor;
+var arrow;
+var arrowTimer = 0;
+var slimeTimer = 3500;
+var princess;
+var stateText;
+        
 function create() {
 
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     game.stage.backgroundColor = '#000000';
 
-    bg = game.add.tileSprite(0, 0, 800, 600, 'background');
-    bg.fixedToCamera = true;
-
     game.physics.arcade.gravity.y = 250;
     
-    //adds sounds to the game//
-    coinSound = game.add.audio('coinCollect');
-    gSound = game.add.audio('gravityChange');
-    coinSound.allowMultiple = true;
-    gSound.allowMultiple = true;
-    coinSound.addMarker('coinGet', 0, 2.0);
-    gSound.addMarker('gSwitch', 0, 0.5);
+    stateText = game.add.text(32, 200, "Game Over! Click to restart!", {font: '34px Arial', fill: '#fff'});
+    stateText.visible = false;
+    
+    floor = game.add.sprite(0, 368, 'floor');
+    game.physics.enable(floor, Phaser.Physics.ARCADE);
+    floor.body.collideWorldBounds = true;
 
-    player = game.add.sprite(32, 32, 'gravityMan');
+    player = game.add.sprite(32, 300, 'knight');
     game.physics.enable(player, Phaser.Physics.ARCADE);
 
-    player.body.bounce.y = 0.2;
     player.body.collideWorldBounds = true;
-    player.body.setSize(16, 32, 8, 0);
+    player.body.setSize(32, 40, 0, 0);
 
-    player.animations.add('left', [2, 3], 10, true);
-    player.animations.add('right', [0, 1], 10, true);
-    player.animations.add('left_up', [6, 7], 10, true);//player is facing left and gravity is up//
-    player.animations.add('right_up', [4, 5], 10, true);//player is facing right and gravity is up//
+    player.animations.add('run', [0, 1, 2, 3], 10, true);
+    player.animations.play('run');
     
-    coins = game.add.group();
-    createCoins();
+    slimes = game.add.group();
     
-    scoreString = 'Score: ';
-    scoreText = game.add.text(10, 10, scoreString + score, {font: '34px Arial', fill: '#fff'});
-
-    game.camera.follow(player);
+    distString = 'Distance: ';
+    distText = game.add.text(10, 10, distString + distance, {font: '34px Arial', fill: '#fff'});
 
     cursors = game.input.keyboard.createCursorKeys();
-    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    shootButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
 }
 
 function update() {
 
     game.physics.arcade.collide(player, floor);
+    game.physics.arcade.collide(slimes, floor);
+    game.physics.arcade.collide(princess, floor);
 
     player.body.velocity.x = 0;
 
-    if (cursors.left.isDown)
+    
+    if (cursors.up.isDown && game.time.now > jumpTimer)
+    {
+        player.body.velocity.y = -150;
+        jumpTimer = game.time.now + 1000;
+    }
+    else if (cursors.left.isDown)
     {
         player.body.velocity.x = -150;
-
-        //if (facing != 'left')
-        //{
-            if(g_dir == 'down')
-                player.animations.play('left');
-            else
-                player.animations.play('left_up');
-            facing = 'left';
-        //}
     }
     else if (cursors.right.isDown)
     {
         player.body.velocity.x = 150;
-
-        //if (facing != 'right')
-        //{
-            if(g_dir == 'down')
-                player.animations.play('right');
-            else
-                player.animations.play('right_up');
-            facing = 'right';
-        //}
     }
+    
+    
+    if (shootButton.isDown && game.time.now > arrowTimer)
+    {
+            arrow = game.add.sprite(player.x, player.y, 'arrow');
+            game.physics.enable(arrow, Phaser.Physics.ARCADE);
+            arrow.body.setSize(15, 32, 0, 0);
+            arrow.body.velocity.x = 500;
+            arrow.body.velocity.y = -40;
+            arrowTimer = game.time.now + 750;
+    }
+    
+    if(game.time.now > slimeTimer && distance > 0)
+    {
+        createSlime();
+        slimeTimer = game.time.now + 2500;
+    }
+    
+    if(distance > 0)
+        distance--;
+    distText.text = distString + distance;
+    
+    game.physics.arcade.overlap(slimes, player, slimeCollision, null, this);
+    game.physics.arcade.overlap(arrow, slimes, arrowCollision, null, this);
+    game.physics.arcade.overlap(princess, player, princessCollision, null, this);
+    
+    if(distance === 0 && princess == null)
+    {
+        princess = game.add.sprite(600, 300, 'princess');
+        game.physics.enable(princess, Phaser.Physics.ARCADE);
+        princess.body.setSize(32, 40, 0, 0);
+        princess.body.velocity.x = -100;
+    }
+    
+    if(princess != null && princess.body.x < 200)
+        princess.body.velocity.x = 0;
+}
+
+//kill player and prompt to restart game//
+function slimeCollision(player, slime)
+{
+    player.kill();
+    stateText.text = "Game Over! Click to restart!";
+    stateText.visible = true;
+    game.input.onTap.addOnce(restart,this);
+}
+        
+function arrowCollision(arrow, slime)
+{
+    arrow.kill();
+    if(slime.body.velocity.x != -100)//will kill any slime faster than Heavy Slime//
+    {
+        slime.kill();
+    }
+}
+        
+function princessCollision(princess, player)
+{
+    stateText.text = "Congradulations, you win!\n click to restart!";
+    stateText.visible = true;
+    game.input.onTap.addOnce(restart, this);
+}
+
+//creates a random slime//
+function createSlime()
+{
+    var num = Math.random() * 3;
+    if(num > 2)
+    {
+        var slime = slimes.create(600, 300, 'slimeSmall');//Basic Slime//
+        game.physics.enable(slime, Phaser.Physics.ARCADE);
+        slime.body.setSize(32, 32, 0, 0);
+        slime.body.velocity.x = -150;
+        slime.animations.add('slime', [0, 1], 3, true);
+        slime.animations.play('slime');
+    }
+    
+    else if(num > 1)
+    {
+        var slime = slimes.create(600, 300, 'slimeHeavy');//Cannot be killed, must jump over//
+        game.physics.enable(slime, Phaser.Physics.ARCADE);
+        slime.body.setSize(32, 32, 0, 0)
+        slime.body.velocity.x = -100;
+        slime.animations.add('slime', [0, 1], 3, true);
+        slime.animations.play('slime');
+    }
+    
     else
     {
-        //if (facing != 'idle')
-        //{
-            player.animations.stop();
-
-            if (facing == 'left')
-            {
-                if(g_dir == 'down')
-                    player.frame = 2;
-                else
-                    player.frame = 6;
-            }
-            else if(facing == 'right')
-            {
-                if(g_dir == 'down')
-                    player.frame = 0;
-                else
-                    player.frame = 4;
-            }
-
-            facing = 'idle';
-        //}
+        var slime = slimes.create(600, 320, 'slimeFlying');//Cannot jump over, must be killed//
+        game.physics.enable(slime, Phaser.Physics.ARCADE);
+        slime.body.setSize(32, 32, 0, 0)
+        slime.body.allowGravity = false;
+        slime.body.velocity.x = -200;
+        slime.animations.add('slime', [0, 1, 2], 5, true);
+        slime.animations.play('slime');
     }
-    
-    if (jumpButton.isDown && game.time.now > jumpTimer)
-    {
-        //player.body.velocity.y = -250;
-        game.physics.arcade.gravity.y *= -1;
-        if (g_dir == 'down')//if gravity is acting downwards, set gravity to 'up'//
-        {
-            g_dir = 'up';
-        }
-        else
-        {
-            g_dir = 'down';
-        }
-        gSound.play('gSwitch');
-        jumpTimer = game.time.now + 750;
-    }
-    
-    //Check for and handles coin collisions//
-    game.physics.arcade.overlap(player, coins, coinCollision, null, this);
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//function createCoins(): spawns 15 coins to random coordinates with random bounce (coins will share the players gravity).  //
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-function createCoins()
-{
-    for(var i = 0; i < 15; i++)
-    {
-        var coin = coins.create((Math.random() * 600) + 1, 16, 'coin');
-        game.physics.enable(coin, Phaser.Physics.ARCADE);
-        coin.body.bounce.y = 1;
-        coin.body.setSize(16, 16, 0, 0);
-        coin.body.collideWorldBounds = true;
-    }
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-//function coinCollision(coin, player)                                                                 //
-//destroys the coin and adds 100 to the score. Spawns another coin with random bounce and x-coordinates//
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-function coinCollision(player, coin)
-{
-    coin.kill();
-    
-    coinSound.play('coinGet');
-    score += 100;
-    scoreText.text = scoreString + score;
-    
-    var newCoin = coins.create((Math.random() * 600) + 1, 16, 'coin');
-    game.physics.enable(newCoin, Phaser.Physics.ARCADE);
-    newCoin.body.bounce.y = 1;
-    newCoin.body.setSize(16, 16, 0, 0);
-    newCoin.body.collideWorldBounds = true;
 }
 
 function render () {
@@ -195,6 +196,17 @@ function render () {
     // game.debug.body(player);
     // game.debug.bodyInfo(player, 16, 24);
 
+}
+//restart game//
+function restart()
+{
+    player.revive();
+    distance = 5000;
+    stateText.visible = false;
+    slimes.removeAll();
+    princess.kill();//Don't worry, she just warped away!//
+    player.body.x = 32;
+    slimeTimer = 3500;
 }
 
     };
